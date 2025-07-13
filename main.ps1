a65afb5ceb98822d898694b206ce12e6bbd6e775
giraen <122036189+giraen@users.noreply.github.com>
2025-07-14 00:12:08 +0800

    Integrated the usb drive detection

--- main.ps1
+++ main.ps1
@@ -1,33 +1,26 @@
-function Search-File{
-    param(
-        [string]$FilePath
-    )
-
-    if (Test-Path $FilePath) {
-        $true
-    } else {
-        $false
-    }
-}
-
 function Write-Logs {
     param(
-        [boolean]$FileExistence,
         [string]$FilePath,
         [string]$Name,
-        [string]$Action,
-        [string]$ActionLocation
+        [string]$Model,
+        [string]$Size,
+        [string]$ActionLocation,
+        [string]$UsbOperation
     )
 
-    $logdate = Get-Date -Format "yyyy/MM/dd HH:mm"
-    if ($FileExistence) {
-        Add-Content -Path $FilePath -Value "$logdate, $Name, $Action, $ActionLocation"
-    } Else {
-        New-item -Path "$FilePath" -ItemType File
-
-        Add-content -Path "$FilePath" -Value "Date, Name, Action, location"
-        Add-Content -Path $FilePath -Value "$logdate, $Name, $Action, $ActionLocation"
+    $timeStamp = (Get-Date).ToString("HH:mm")
+    if ($UsbOperation) {
+        $action = "$Model($Size) $UsbOperation"
+    } else {
+        $action = "this is meant for file operations!"
     }
+    
+    [PSCustomObject]@{
+        Timestamp = $timeStamp
+        Name      = $Name
+        Action    = $action
+        ActionLocation = $ActionLocation
+    } | Export-Csv -Path "$FilePath" -NoTypeInformation -Append
 }
 
 function Show-InputBox {
@@ -35,25 +28,129 @@ function Show-InputBox {
     $global:name = [Microsoft.VisualBasic.Interaction]::InputBox("Please enter your name: ", "Name Logger")
 }
 
-$currDate = Get-Date -Format 'yyMMdd'
-$fileName = "$currDate-Logs.csv"
-$logFileNamePath = ".\Logs\$fileName"
-$flashDriveHash = @{}
-$loggedSerials = @{}
+function Get-FlashDrive {
+    $flashDrives = @(Get-WmiObject Win32_DiskDrive `
+        | Where-Object { $_.MediaType -like "*Removable*" } `
+        | Select-Object Model, SerialNumber, Size)
 
-$name = "Unknown"
-$actionLocation =  "D:\Programming\02_Prototype\logger\PastingDirectory"
+    foreach ($flashDrive in $flashDrives) {
+        $serial = $flashDrive.SerialNumber
+        $model = $flashDrive.Model
+        $size = $flashDrive.Size
 
-#S1
-#S2
+        # Skip if already logged
+        if ($global:loggedSerials.ContainsKey($serial)) {
+            continue
+        }
 
-# $logFileExists = Search-File -FilePath $logFileNamePath
-# If ($isScenario1) {
-#     $actionPerformed = "'$flashDriveName' mounted"
-#     Show-InputBox
-#     Write-Logs -FileExistence $logFileExists -FilePath "$logFileNamePath" -Name "$name" -Action "$actionPerformed"
-# }
+        # Add them to the hashtable if not yet added
+        if (-not $global:flashDriveHash.ContainsKey($serial)) {
+            # Create a value object containing model and size
+            $driveInfo = [PSCustomObject]@{
+                Model = $model
+                Size  = $size
+            }
 
-# If ($isScenario2) {
-#     Write-Logs -FileExistence $logFileExists -FilePath "$logFileNamePath" -Name "$name" -Action "$actionPerformed" -actionLocation "$actionLocation"
-# }
+            # Mark the newly added serial to be logged (true)
+            $global:loggedSerials[$serial] = $true
+            $global:flashDriveHash[$serial] = $driveInfo 
+
+            Write-Logs -FilePath $global:logFileNamePath `
+                       -Name $global:name `
+                       -Model $model `
+                       -Size $size `
+                       -UsbOperation "Inserted" `
+                       -ActionLocation ""
+        }
+    }
+}
+
+function Remove-FlashDrive {
+    $serialNums = [System.Collections.Generic.List[string]]::new()
+    $serialNumsToRemove = @()
+
+    $flashDrives = @(Get-WmiObject Win32_DiskDrive `
+        | Where-Object { $_.MediaType -like "*Removable*" } `
+        | Select-Object Model, SerialNumber, Size)
+
+    if ($flashDrives.Count -eq 0) {
+        foreach ($serial in $global:flashDriveHash.Keys) {
+            $driveInfo = $global:flashDriveHash[$serial]
+
+            Write-Logs -FilePath $global:logFileNamePath `
+                       -Name $global:name `
+                       -Model $driveInfo.Model `
+                       -Size $driveInfo.Size `
+                       -UsbOperation "Removed" `
+                       -ActionLocation ""
+        }
+
+        # Clear everything
+        $global:flashDriveHash.Clear()
+        $global:loggedSerials.Clear()
+        return
+    }
+
+    # Get the current connected flash drives
+    foreach ($flashDrive in $flashDrives) {
+        $serialNums.Add($flashDrive.SerialNumber)
+    }
+
+    # Check if the connected flash drives are in the hashtable
+    # If not, then add them to an array of keys to be removed
+    foreach ($prevSerialNum in $global:flashDriveHash.Keys) {
+        if (-not $serialNums.Contains($prevSerialNum)) {
+            $serialNumsToRemove.Add($prevSerialNum)
+        }
+    }
+
+    # Iterate over the keys to be removed and remove them from the hashtable
+    foreach ($serialNumToRemove in $serialNumsToRemove) {
+        $driveInfo = $global:flashDriveHash[$serialNumToRemove]
+
+        Write-Logs -FilePath $global:logFileNamePath `
+                   -Name $global:name `
+                   -Model $driveInfo.Model `
+                   -Size $driveInfo.Size `
+                   -UsbOperation "Removed" `
+                   -ActionLocation ""
+
+        $global:flashDriveHash.Remove($serialNumToRemove)
+        $global:loggedSerials.Remove($serialNumToRemove)
+    }
+}
+
+$global:currDate = Get-Date -Format 'yyMMdd'
+$global:fileName = "$global:currDate-Logs.csv"
+$global:logFileNamePath = ".\Logs\$global:fileName"
+$global:flashDriveHash = @{}
+$global:loggedSerials = @{}
+
+$global:name = "Unknown"
+$global:actionLocation =  "D:\Programming\02_Prototype\logger\PastingDirectory"
+
+Register-WmiEvent -Class Win32_DeviceChangeEvent -SourceIdentifier deviceChange -Action {
+    $eventData = $Event.SourceEventArgs.NewEvent
+
+    switch ($eventData.EventType) {
+        2 { 
+            # Flash drive inserted
+            Get-FlashDrive
+        }
+        3 { 
+            # Flash drive removal
+            Remove-FlashDrive
+        }
+    }
+}
+
+Write-Host "USB drive detector running. Press Ctrl+C to exit."
+try {
+    while($true) {
+        Start-Sleep -Seconds 5
+    }
+}
+finally {
+    Write-Host "`nStopping USB drive detector..."
+    Unregister-Event -SourceIdentifier deviceChange
+}
